cef28bc63d2132b1c646f7a4729589545218af02
Alberto Trindade Tavares <alberto.tavares@pe.senai.br>
2016-11-02 20:01:25 -0300

    Add new test cases

--- test/mock/github-mock.js
+++ test/mock/github-mock.js
@@ -12,5 +12,11 @@ module.exports = function githubMock() {
                    `${__dirname}/github_responses/63d6fe90f434533b5a52edcd93d9786b8c1834e8.json`)
     .get('/repos/google/closure-compiler/commits/6b158f92af743c0a89e9b343af9fe95c08d31bef')
     .replyWithFile(200,
-                   `${__dirname}/github_responses/6b158f92af743c0a89e9b343af9fe95c08d31bef.json`);
+                   `${__dirname}/github_responses/6b158f92af743c0a89e9b343af9fe95c08d31bef.json`)
+    .get('/repos/nathanl/searchlight/commits/94a06293db0f37bda1b72e5693a58ed94f184936')
+    .replyWithFile(200,
+                   `${__dirname}/github_responses/94a06293db0f37bda1b72e5693a58ed94f184936.json`)
+    .get('/repos/nathanl/searchlight/commits/04e3541e39312eef8ac71cf2476ba8feb2d4b88b')
+    .replyWithFile(200,
+                   `${__dirname}/github_responses/04e3541e39312eef8ac71cf2476ba8feb2d4b88b.json`);
 };
--- test/tdd-detector-test.js
+++ test/tdd-detector-test.js
@@ -12,7 +12,7 @@ describe('TDD Detector isTDDUsed', function() {
     githubMock();
   });
 
-  it('should return true when we have pairs in same commit', function() {
+  it('should return true when we have pairs in the same commit', function() {
     const projectAuthor = 'google';
     const projectName = 'closure-compiler';
     const language = 'java';
@@ -21,6 +21,30 @@ describe('TDD Detector isTDDUsed', function() {
     return tddDetector.isTDDUsed(projectAuthor, projectName, language, commits)
       .then(result => {
         expect(result).to.be.true;
+      });
+  });
+
+  it('should return true when we have pairs in different commits, but with tests before/along with implementation', function() {
+    const projectAuthor = 'nathanl';
+    const projectName = 'searchlight';
+    const language = 'ruby';
+    const commits = ['94a06293db0f37bda1b72e5693a58ed94f184936', '04e3541e39312eef8ac71cf2476ba8feb2d4b88b']
+
+    return tddDetector.isTDDUsed(projectAuthor, projectName, language, commits)
+      .then(result => {
+        expect(result).to.be.true;
+      });
+  });
+
+  it('should return false when we do not have pairs with tests before/along with implementation', function() {
+    const projectAuthor = 'google';
+    const projectName = 'closure-compiler';
+    const language = 'java';
+    const commits = ['59f5f9c484164fc394cd5d34485ad7f4c37bc21e', '6b158f92af743c0a89e9b343af9fe95c08d31bef']
+
+    return tddDetector.isTDDUsed(projectAuthor, projectName, language, commits)
+      .then(result => {
+        expect(result).to.be.false;
       });
   });
 });
